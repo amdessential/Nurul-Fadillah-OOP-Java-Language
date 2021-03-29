44562fa1f0f5c72c1e94ee017d0084ec81163465
Dilaa <66761532+amdessential@users.noreply.github.com>
2021-03-29 22:36:48 +0700

    Add files via upload

---
+++ NilaiDitentukan.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week5lab;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class NilaiDitentukan {
+    
+     public static void main(String[] args){
+
+        Tabung objek = new Tabung();
+        double r = objek.radius = 5;
+        double t = objek.tinggi = 10;
+
+        System.out.println("== Nilai Radius 5 & Tinggi 10 ==");
+        System.out.println("Nilai radius di Nilai default: "+ r );
+        System.out.println("Nilai Tinggi di Nilai default: "+ t );
+        System.out.println("Nilai Luas Alas Tabung: "+ objek.getLuasAlas());
+        System.out.println("Nilai Volume Tabung: "+ objek.getVolume());
+
+        Tabung objek2 = new Tabung();
+        double r2 = objek2.radius = 7.5;
+        double t2 = objek2.tinggi = 15.5;
+
+        System.out.println("\n\n== Nilai Radius 7.5 & Tinggi 15.5 ==");
+        System.out.println("Nilai radius di Nilai default: "+ r2 );
+        System.out.println("Nilai Tinggi di Nilai default: "+ t2 );
+        System.out.println("Nilai Luas Alas Tabung: "+ objek2.getLuasAlas());
+        System.out.println("Nilai Volume Tabung: "+ objek2.getVolume());
+    }
+     
+}
